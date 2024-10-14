82b553949b61ae59f782ee2a716b31cfee97c9bf
hnasit <c0894125@mylambton.ca>
2024-10-13 22:32:56 -0400

    Added Login UI

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,22 @@ import { Provider } from "react-redux";
 import Store from "./store/Store";
 import SignUp from "./components/auth/Signup";
 import ProtectedRoutes from "./ProtectedRoutes";
+import Login from "./components/auth/Login";
+import Dashboard from "./components/Dashboard";
 
 function App() {
   return (
     <Provider store={Store}>
       <Router>
         <Routes>
+          <Route path="/" element={<Navigate to="/login" />} />
+          <Route exact path="/login" element={<Login />} />
           <Route exact path="/signup" element={<SignUp />} />
-          <Route path="/main" element={<ProtectedRoutes Component={Main} />} />
+          <Route path="/dashboard" element={<ProtectedRoutes Component={Dashboard} />} />
         </Routes>
       </Router>
     </Provider>
   );
 }
-
-const Main = () => {
-  return <h1>MAIN</h1>;
-};
 
 export default App;
