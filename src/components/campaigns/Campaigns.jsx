ff3361ab939c712ba62c053f6f32022624a3ae2c
hnasit <c0894125@mylambton.ca>
2024-11-01 20:56:39 -0400

    Added REgistration UI

--- src/components/campaigns/Campaigns.jsx
+++ src/components/campaigns/Campaigns.jsx
@@ -1,24 +1,56 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import ViewCampaign from "./view/ViewCampaign";
-import { Toolbar, Typography, Button } from "@mui/material";
+import { Toolbar, Typography, Button, Tabs, Tab, Box } from "@mui/material";
 import CreateCampaign from "./create/CreateCampaign";
-import { ADDCAMPAIGNSDATA, EDITCAMPAIGNSDATA } from "../../actions/campaigns/ActionCreators";
+import { ADDCAMPAIGNSDATA, EDITCAMPAIGNSDATA, REGISTERFORCAMPAGIN } from "../../actions/campaigns/ActionCreators";
 import UpdateCampaign from "./update/UpdateCampaign";
+import ViewHistoricalCampaign from "./view/ViewHistoricalCampaign";
+import CampaignDetails from "./details/CampaignDetails";
+
+function TabPanel({ children, value, index, ...other }) {
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
+    </div>
+  );
+}
 
 function Campaigns() {
   const dispatch = useDispatch();
 
   const [createModal, setCreateModal] = useState(false);
   const [updateModal, setUpdateModal] = useState(false);
+  const [detailsModal, setDetailsModal] = useState(false);
+
   const [selectedRow, setSelectedRow] = useState(false);
+  const [activeTab, setActiveTab] = useState(0);
+
+  {
+    /* -------------------------------- handle various modals --------------------------------- */
+  }
 
   const handleCreateModal = () => setCreateModal(!createModal);
 
-  const handleUpdateModal = (obj={}) => {
+  const handleUpdateModal = (obj = {}) => {
     setSelectedRow(obj);
     setUpdateModal(!updateModal);
   };
+
+  const handleDetailsModal = (obj = {}) => {
+    setSelectedRow(obj);
+    setDetailsModal(!detailsModal);
+  };
+
+  {
+    /* -------------------------------- handle api calls --------------------------------- */
+  }
 
   const handleCreateCampaign = (obj) => {
     dispatch(ADDCAMPAIGNSDATA(obj, handleCreateModal));
@@ -28,18 +60,63 @@ function Campaigns() {
     dispatch(EDITCAMPAIGNSDATA(obj?._id, obj, handleUpdateModal));
   };
 
+  const handleCampaignRegistration = (id) => {
+    dispatch(REGISTERFORCAMPAGIN(id, localStorage.getItem("userId"), handleDetailsModal));
+  };
+
+  // Handling tab change
+  const handleTabChange = (event, newValue) => {
+    setActiveTab(newValue);
+  };
+
   // Memoizing the ViewCampaign component
   const MemoizedViewCampaign = React.memo(ViewCampaign);
 
+  // Memoizing the ViewCampaign component
+  const MemoizedViewHistoricalCampaign = React.memo(ViewHistoricalCampaign);
+
   return (
-    <div>
-      <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
+    <Box sx={{ width: "100%" }}>
+      <Toolbar sx={{ display: "flex", justifyContent: "space-between", width: "100%" }}>
         <Typography variant="h6">All Campaigns</Typography>
+
+        {/* -------------------------------- Tabs --------------------------------- */}
+
+        <Tabs
+          value={activeTab}
+          onChange={handleTabChange}
+          centered
+          sx={{
+            "& .MuiTabs-flexContainer": { display: "flex", alignItems: "center" },
+            "& .MuiTab-root:focus-visible": { outline: "none" }, // Remove focus outline
+          }}
+        >
+          <Tab label="Current" sx={{ "&:focus-visible": { outline: "none" } }} />
+          <Tab label="Historical" sx={{ "&:focus-visible": { outline: "none" } }} />
+        </Tabs>
+
         <Button variant="contained" color="secondary" onClick={handleCreateModal}>
           Create
         </Button>
       </Toolbar>
-      <MemoizedViewCampaign handleUpdateModal={handleUpdateModal} />
+
+      {/* -------------------------------- Tab Panel ---------------------------------- */}
+
+      <TabPanel value={activeTab} index={0}>
+        <MemoizedViewCampaign
+          handleUpdateModal={handleUpdateModal}
+          handleDetailsModal={handleDetailsModal}
+        />
+      </TabPanel>
+      <TabPanel value={activeTab} index={1}>
+        <MemoizedViewHistoricalCampaign
+          handleUpdateModal={handleUpdateModal}
+          handleDetailsModal={handleDetailsModal}
+        />
+      </TabPanel>
+
+      {/* -------------------------------- Create Campaign ---------------------------------- */}
+
       {createModal && (
         <CreateCampaign
           open={createModal}
@@ -47,15 +124,29 @@ function Campaigns() {
           onCreate={handleCreateCampaign}
         />
       )}
+
+      {/* -------------------------------- Update Campaign ---------------------------------- */}
+
       {updateModal && (
         <UpdateCampaign
-        campaignData={selectedRow}
+          campaignData={selectedRow}
           open={updateModal}
           onClose={handleUpdateModal}
           onUpdate={handleUpdateCampaign}
         />
       )}
-    </div>
+
+      {/* -------------------------------- Campaign Details ---------------------------------- */}
+
+      {detailsModal && (
+        <CampaignDetails
+          campaignData={selectedRow}
+          open={detailsModal}
+          onClose={handleDetailsModal}
+          onCreate={handleCampaignRegistration}
+        />
+      )}
+    </Box>
   );
 }
 
---
+++ src/components/campaigns/details/CampaignDetails.jsx
@@ -0,0 +1,110 @@
+import React from "react";
+import {
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  Grid,
+  Box,
+  Typography,
+  Button,
+  FormControl,
+  FormLabel,
+  Checkbox,
+} from "@mui/material";
+
+const CampaignDetails = ({ open, onClose, campaignData, onCreate }) => {
+  return (
+    <Dialog open={open} onClose={onClose} maxWidth="md" fullWidth>
+      <DialogTitle>
+        <Typography variant="h5" sx={{ fontWeight: "bold", textAlign: "center" }}>
+          Campaign Details
+        </Typography>
+      </DialogTitle>
+
+      <DialogContent>
+        <Box sx={{ mt: 2 }}>
+          <Grid container spacing={3}>
+            <Grid item xs={12}>
+              <FormControl fullWidth>
+                <FormLabel sx={{ mb: 1, fontWeight: "bold" }}>Campaign Title</FormLabel>
+                <Typography variant="body1">{campaignData.title}</Typography>
+              </FormControl>
+            </Grid>
+
+            <Grid item xs={12}>
+              <FormControl fullWidth>
+                <FormLabel sx={{ mb: 1, fontWeight: "bold" }}>Description</FormLabel>
+                <Typography
+                  variant="body1"
+                  sx={{
+                    whiteSpace: "pre-line",
+                    maxHeight: 150,
+                    overflow: "auto",
+                  }}
+                >
+                  {campaignData.description}
+                </Typography>
+              </FormControl>
+            </Grid>
+
+            <Grid item xs={6}>
+              <FormControl fullWidth>
+                <FormLabel sx={{ mb: 1, fontWeight: "bold" }}>Target Amount</FormLabel>
+                <Typography variant="body1">{`$${campaignData.targetAmount?.toLocaleString()}`}</Typography>
+              </FormControl>
+            </Grid>
+
+            <Grid item xs={6}>
+              <FormControl fullWidth>
+                <FormLabel sx={{ mb: 1, fontWeight: "bold" }}>Raised Amount</FormLabel>
+                <Typography variant="body1">{`$${campaignData.raisedAmount?.toLocaleString()}`}</Typography>
+              </FormControl>
+            </Grid>
+
+            <Grid item xs={6}>
+              <FormControl fullWidth>
+                <FormLabel sx={{ mb: 1, fontWeight: "bold" }}>Start Date</FormLabel>
+                <Typography variant="body1">
+                  {new Date(campaignData.startDate).toLocaleDateString()}
+                </Typography>
+              </FormControl>
+            </Grid>
+
+            <Grid item xs={6}>
+              <FormControl fullWidth>
+                <FormLabel sx={{ mb: 1, fontWeight: "bold" }}>End Date</FormLabel>
+                <Typography variant="body1">
+                  {new Date(campaignData.endDate).toLocaleDateString()}
+                </Typography>
+              </FormControl>
+            </Grid>
+
+            <Grid item xs={12}>
+              <FormControl fullWidth>
+                <FormLabel sx={{ mb: 1, fontWeight: "bold" }}>Active Campaign</FormLabel>
+                <Checkbox
+                  checked={campaignData.isActive}
+                  readOnly
+                  sx={{ p: 0, ml: 1 }}
+                  disableRipple
+                />
+              </FormControl>
+            </Grid>
+          </Grid>
+        </Box>
+      </DialogContent>
+
+      <DialogActions sx={{ justifyContent: "center", pb: 2 }}>
+        <Button onClick={onClose} variant="outlined" color="secondary">
+          Close
+        </Button>
+        <Button onClick={() => onCreate(campaignData._id)} variant="contained" color="primary">
+          Register
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+export default CampaignDetails;
